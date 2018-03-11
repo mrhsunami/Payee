b3d2e8f954fa6e20c2a816ef3f9148142c9b03b6
Christoper <celoranta@gmail.com>
2018-03-11 12:29:20 -0700

    fixed bug where payer's ccontribution was not recorded if not listed in buddies list

--- Payee/TripManager.swift
+++ Payee/TripManager.swift
@@ -23,17 +23,16 @@ class TripManager: CanCreateTransactionDelegate {
     // Note, if the payer is not listed among the payees, his payment will currently not be recorded.
     let meal: Meal = Meal.init(name: transactionName, andExpense: expenseAmount)
     let payer: Buddy = Buddy.newOr(matchingNameString: payerName)
+    let _: LedgerLine = LedgerLine.init(trip: activeTrip, meal: meal, buddy: payer, tab: 0.00, andPay: expenseAmount)
     for buddyNameAndPortion in arrayOfbuddyNamesAndPortions
     {
       let buddyName = buddyNameAndPortion.0
       let buddy: Buddy = Buddy.newOr(matchingNameString: buddyName)
-      var pay: Float = 0.00;
-      if(payer.isEqual(to: buddy))
-      {
-        pay = expenseAmount
-      }
+      let pay: Float = 0.00;
       let _: LedgerLine = LedgerLine.init(trip: activeTrip, meal: meal, buddy: buddy, tab: buddyNameAndPortion.1, andPay: pay)
     }
+
+    
   }
 }
 
